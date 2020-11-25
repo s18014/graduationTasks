3e05046243c15ba14d49dd669d7fc12dca97ea7b
s18014 <s18014@std.it-college.ac.jp>
2020-11-25 15:38:30 +0900

    Add mybatis's configurations. I don't know DI system will... Maybe AppConfig use DataSource from aguments that was Added DI.

---
+++ sketchoogiri/src/main/java/sketchoogiri/AppConfig.java
@@ -0,0 +1,35 @@
+package sketchoogiri;
+
+import javax.sql.DataSource;
+
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@ComponentScan("com.example.domain")
+@EnableTransactionManagement
+@MapperScan("com.example.domain.mapper")
+public class AppConfig {
+	
+	// 多分データソースはapplication.propatiesのdatasourceをDIしてくれる
+	@Bean
+	public PlatformTransactionManager transactionManager(DataSource dataSource) {
+		return new DataSourceTransactionManager(dataSource);
+	}
+	
+	@Bean
+	public SqlSessionFactoryBean sqlSessionFactory(DataSource dataSource) {
+		SqlSessionFactoryBean sessionFactoryBean = new SqlSessionFactoryBean();
+		sessionFactoryBean.setDataSource(dataSource);
+		sessionFactoryBean.setConfigLocation(
+				new ClassPathResource("/mybatis-config.xml"));
+		return sessionFactoryBean;
+	}
+}
